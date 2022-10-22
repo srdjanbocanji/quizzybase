96adc23f8f0f8bcadac5eee2c1d5a03ee4059c88
sbocanji <bocanisrdjan@gmail.com>
2022-10-22 22:33:29 +0200

    initial commit

---
+++ src/main/java/com/bocanjis/quizzybase/QuizzybaseApplication.java
@@ -0,0 +1,13 @@
+package com.bocanjis.quizzybase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzybaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzybaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bocanjis/quizzybase/QuizzybaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bocanjis.quizzybase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzybaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
